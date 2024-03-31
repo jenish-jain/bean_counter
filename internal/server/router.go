c23f901f0a98585084c4e914b20466a16f6e0a52
jenish-jain <jenishjain@rocketmail.com>
2024-03-31 17:27:56 +0530

    upgraded logger

--- internal/server/router.go
+++ internal/server/router.go
@@ -3,22 +3,13 @@ package server
 import (
 	"bean_counter/internal/reporter"
 
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/jenish-jain/logger"
 )
 
 func (s *Server) InitRoutes() {
 	router := s.routerGroups.rootRouter
-	router.Use(requestIdMiddleWare)
+	router.Use(logger.AttachRequestIdToRequests)
 
 	reporterHandler := reporter.NewHandler()
 	reporterHandler.InitRoutes(router)
-}
-
-func requestIdMiddleWare(c *gin.Context) {
-	u := uuid.New()
-	c.Set("request_id", u)
-	logger.DebugWithCtx(c, "request started", "method", c.Request.Method, "path", c.Request.URL.Path)
-	c.Next()
 }
